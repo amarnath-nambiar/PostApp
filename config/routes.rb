14853fe3c3603c5c18bfcdad1bc6b43556b28bbf
Joshi <amarnath@foradian.com>
2019-07-20 18:54:14 +0530

    basic setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :replies
   devise_for :users
   resources :comments
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   resources :users
   root to: "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
