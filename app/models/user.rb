f24fce31ec5e8b1d69633a3bd9ca51c329bdea09
MohanThanigaivelan <mohan@rently.com>
2019-07-28 15:02:42 +0530

    Restricted Access

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
-
+  has_many :posts
   def full_name
 
   end
