9d028fb4187fa4d952a793d69791b8faf5ef3da1
Joshi <amarnath@foradian.com>
2019-07-31 19:06:38 +0530

    initial setups

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,13 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.includes(:user)
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @comments = @post.comments
+    @comments = @post.comments.order(updated_at: :desc).includes(:user)
   end
 
   # GET /posts/new
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-
+  has_many :comments
   def full_name
 
   end
